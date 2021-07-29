3ab94d8cf9f067707005b9c4c5985ec88e0e1aa0
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-07-29 10:54:41 -0500

    Created many to one relationship

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,21 +3,24 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
  * Created by Chris Bay
  */
 @Entity
-public class EventCategory {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class EventCategory extends AbstractEntity{
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    // workshop object has id of 4 and name of "Workshop"
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
@@ -33,25 +36,12 @@ public class EventCategory {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
     public String toString() {
         return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EventCategory that = (EventCategory) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
